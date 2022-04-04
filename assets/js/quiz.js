903878f62ee36b51b9e95a7adb4ee744f795de50
Shawn Argent <kshawn001@gmail.com>
2022-04-04 09:00:47 -0700

    commit

---
+++ assets/js/quiz.js
@@ -0,0 +1,113 @@
+const questions = [
+	{
+	question: "What is the world's most popular programming language?", 
+	choices: [
+		 "a. HTML",
+		 "b. CSS",
+		 "c. JavaScript",
+		 "d. Java"
+		],
+        answer: "c. JavaScript", 
+	},
+	  {
+	question: "What is the correct JavaScript syntax to change the content of an HTML element?",
+	choices: [
+		 'a. document.getElementByName("p").innerHTML = "Hello World!"',
+		 'b. document.getElement("p").innerHTML = "Hello World!"',
+		 'c. #demo.innerHTML = "Hello World!"',
+		 'd. document.getElementById("demo").innerHTML = "Hello World!";'
+		],
+        answer: 'd. document.getElementById("demo").innerHTML = "Hello World!";', 
+	},
+        {
+	question: 'How do you write "Hello World" in an alert box?', 
+	choices: [
+		'a. alertBox("Hello World")',
+		 'b. msg("Hello World")',
+		 'c. alert("Hello World");',
+		 'd. msgBox("Hello World")'
+		],
+        answer: 'c. alert("Hello World");',
+	},
+	  {
+	question: "How do you create a function in JavaScript?", 
+	choices: [
+		 "a. function = myFunction()",
+		 "b. function myFunction()",
+		 "c. function:myFunction()",
+		 "d. callFunction myFunction()"
+	
+		],
+        answer: "b. function myFunction()",
+	},
+	  {
+	question: "How to write an IF statement in JavaScript?", 
+	choices: [
+		 "a. if i = 5 then",
+		 "b. if i = 5",
+		 "c. if (i == 5)",
+		 "d. if i == 5 then"
+	
+		],
+        answer: "c. if (i == 5)", 
+	},
+	  {
+	question: "How does a WHILE loop start?", 
+
+	choices: [
+		 "a. while i = 1 to 10",
+		 "b. while (i <= 10; i++)",
+		 "c. while (i < 10)",
+		 "d. while i = 10++)"
+	
+		],
+        answer: "c. while (i < 10)", 
+	},
+	  {
+	question: 'What is a correct syntax to output "Hello World" in Java?', 
+	 
+	choices: [
+		 'a. print ("Hello World");',
+		 'b. Console.WriteLine("Hello World");',
+		 'c. echo("Hello World:);',
+		 'd. System.out.printIn("Hello World");'
+	
+		],
+        answer: 'd. System.out.printIn("Hello World");'
+	},
+	  {
+	question: "How do you add a comment in a JavaScript file?", 
+	choices: [
+		 "a. !--This is a commnt--",
+		 'b. "This is a comment"',
+		 "c. =This is a comment=",
+		 "d. //This is a comment"
+    ],
+    answer: "d. //This is a comment", 
+},
+    {
+    question: "Which data type is used to create a variable that should store text?",
+    choices: [
+        "a. string",
+        "b. Txt",
+        "c. String",
+        "d. myString"
+   
+       ],
+       answer: "c. String",
+   },
+     {
+        question: "How do you create a variable with the numeric value 5?",
+        choices: [
+            "a. x=5;",
+            "b. float x=5;",
+            "c. num x=5;",
+            "d. int x=5;"
+       
+           ],
+           answer: "a. x=5;",
+       
+	},
+];
+
+
