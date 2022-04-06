b7023a0fac85a0d5745aa96ec6b5ba56a2da1e2f
Shawn Argent <kshawn001@gmail.com>
2022-04-06 00:11:21 -0700

    first submit

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,9 @@
+//global variables
+var intervalID;
+var time;
+var currentQuestion;
+
+//quiz questions
 const questions = [
 	{
 	question: "What is the world's most popular programming language?", 
@@ -110,4 +116,220 @@ const questions = [
 	},
 ];
 
+//card variables
+const startCard = document.querySelector("#start-card");
+const questionCard = document.querySelector("#question-card");
+const scoreCard = document.querySelector("#score-card");
+const leaderboardCard = document.querySelector("#leaderboard-card");
 
+function hideCards() {
+  startCard.setAttribute("hidden", true);
+  questionCard.setAttribute("hidden", true);
+  scoreCard.setAttribute("hidden", true);
+  leaderboardCard.setAttribute("hidden", true);
+}
+
+const resultDiv = document.querySelector("#result-div");
+const resultText = document.querySelector("#result-text");
+
+
+function hideResultText() {
+  resultDiv.style.display = "none";
+}
+
+document.querySelector("#start-button").addEventListener("click", startQuiz);
+
+function startQuiz() {
+ 
+  hideCards();
+  questionCard.removeAttribute("hidden");
+
+  currentQuestion = 0;
+  displayQuestion();
+
+  //sets total time 
+  time = questions.length * 10;
+
+  
+  intervalID = setInterval(countdown, 1000);
+
+ 
+  displayTime();
+}
+
+//time display
+function countdown() {
+  time--;
+  displayTime();
+  if (time < 1) {
+    endQuiz();
+  }
+}
+
+const timeDisplay = document.querySelector("#time");
+function displayTime() {
+  timeDisplay.textContent = time;
+}
+
+//display question and answer choices
+function displayQuestion() {
+  let question = questions[currentQuestion];
+  let choices = question.choices;
+
+  let h2QuestionElement = document.querySelector("#question-text");
+  h2QuestionElement.textContent = question.question;
+
+  for (let i = 0; i < choices.length; i++) {
+    let choice = choices[i];
+    let choiceButton = document.querySelector("#choice" + i);
+    choiceButton.textContent = choice;
+  }
+}
+
+document.querySelector("#quiz-choices").addEventListener("click", checkAnswer);
+
+
+function choiceIsCorrect(choiceButton) {
+  return choiceButton.textContent === questions[currentQuestion].answer;
+}
+
+//incorrect answer subtract time
+function checkAnswer(eventObject) {
+  let choiceButton = eventObject.target;
+  resultDiv.style.display = "block";
+  if (choiceIsCorrect(choiceButton)) {
+    resultText.textContent = "Correct!";
+    setTimeout(hideResultText, 1000);
+  } else {
+    resultText.textContent = "Incorrect!";
+    setTimeout(hideResultText, 1000);
+    if (time >= 10) {
+      time = time - 10;
+      displayTime();
+    } else {
+      time = 0;
+      displayTime();
+      endQuiz();
+    }
+  }
+
+  //quiz ends after all questions answered
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+//score total
+const score = document.querySelector("#score");
+
+function endQuiz() {
+  clearInterval(intervalID);
+  hideCards();
+  scoreCard.removeAttribute("hidden");
+  score.textContent = time;
+}
+
+const submitButton = document.querySelector("#submit-button");
+const inputElement = document.querySelector("#initials");
+
+//store score
+submitButton.addEventListener("click", storeScore);
+
+function storeScore(event) {
+  event.preventDefault();
+
+  if (!inputElement.value) {
+    alert("Enter initials before pressing submit!");
+    return;
+  }
+
+  let leaderboardItem = {
+    initials: inputElement.value,
+    score: time,
+  };
+
+  updateStoredLeaderboard(leaderboardItem);
+
+  //highscore leaderboard
+  hideCards();
+  leaderboardCard.removeAttribute("hidden");
+
+  renderLeaderboard();
+}
+
+function updateStoredLeaderboard(leaderboardItem) {
+  let leaderboardArray = getLeaderboard();
+  //append new leaderboard item to leaderboard array
+  leaderboardArray.push(leaderboardItem);
+  localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+}
+
+function getLeaderboard() {
+  let storedLeaderboard = localStorage.getItem("leaderboardArray");
+  if (storedLeaderboard !== null) {
+    let leaderboardArray = JSON.parse(storedLeaderboard);
+    return leaderboardArray;
+  } else {
+    leaderboardArray = [];
+  }
+  return leaderboardArray;
+}
+
+function renderLeaderboard() {
+  let sortedLeaderboardArray = sortLeaderboard();
+  const highscoreList = document.querySelector("#highscore-list");
+  highscoreList.innerHTML = "";
+  for (let i = 0; i < sortedLeaderboardArray.length; i++) {
+    let leaderboardEntry = sortedLeaderboardArray[i];
+    let newListItem = document.createElement("li");
+    newListItem.textContent =
+      leaderboardEntry.initials + " - " + leaderboardEntry.score;
+    highscoreList.append(newListItem);
+  }
+}
+
+function sortLeaderboard() {
+  let leaderboardArray = getLeaderboard();
+  if (!leaderboardArray) {
+    return;
+  }
+
+  leaderboardArray.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  return leaderboardArray;
+}
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", clearHighscores);
+
+function clearHighscores() {
+  localStorage.clear();
+  renderLeaderboard();
+}
+
+const backButton = document.querySelector("#back-button");
+backButton.addEventListener("click", returnToStart);
+
+function returnToStart() {
+  hideCards();
+  startCard.removeAttribute("hidden");
+}
+
+const leaderboardLink = document.querySelector("#leaderboard-link");
+leaderboardLink.addEventListener("click", showLeaderboard);
+
+function showLeaderboard() {
+  hideCards();
+  leaderboardCard.removeAttribute("hidden");
+
+  clearInterval(intervalID);
+
+  time = undefined;
+  displayTime();
+
+  renderLeaderboard();
+}
